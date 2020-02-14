68120afae8fe0504a3156be65b8d303692dd9ae5
eloisa.fagundes <lola.fernanda@hotmail.com>
2020-02-14 17:14:16 -0300

    Escopo 2 - Envio da Mensagem, concluído

--- whats-projeto/src/App.js
+++ whats-projeto/src/App.js
@@ -1,14 +1,65 @@
 import React from 'react';
 import './App.css';
 import TelaDoUsuario from './Components/TelaDoUsuario/TelaDoUsuario'
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <TelaDoUsuario />
-    
-    </div>
-  );
+const Container = styled.div`
+border: 1px solid gray;
+display: flex;
+margin: auto;
+height: 100vh;
+width: 350px;
+flex-direction: column;
+`
+
+const AreaDaMensagem = styled.div`
+height:90%;
+`
+const Usuario = styled.span`
+font-weight: bold;
+`
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      testeDaMensagem: []
+    }
+
+  }
+
+  AdicionarMensagem = (mensagem) => {
+    const copiaArrayDeMensagens = [... this.state.testeDaMensagem]
+    copiaArrayDeMensagens.push(mensagem)
+    this.setState({
+      testeDaMensagem: copiaArrayDeMensagens
+    })
+    console.log(this.state.testeDaMensagem)
+  }
+
+  render() {
+    return (
+      <Container>
+
+        <AreaDaMensagem>
+          {
+            this.state.testeDaMensagem.map((element, index) => {
+              return (
+                <p key={index}>
+                  <Usuario> {element.usuario + ' :'} </Usuario>
+                  {element.mensagem}</p>)
+            })
+          }
+        </AreaDaMensagem>
+
+        <TelaDoUsuario AdicionarMensagem={this.AdicionarMensagem} />
+
+
+      </Container>
+
+    )
+  }
 }
+
 
 export default App;
--- whats-projeto/src/Components/TelaDoUsuario/TelaDoUsuario.js
+++ whats-projeto/src/Components/TelaDoUsuario/TelaDoUsuario.js
@@ -1,24 +1,12 @@
 import React from 'react';
 import styled from 'styled-components'
 
-const Container = styled.div`
-border: 1px solid red;
-display: flex;
-height: 100vh;
-width: 350px;
-flex-direction: column;
-`
 
-const CampoDosInputs = styled.div`
-width: 100%;
-`
-
-const AreaDaMensagem = styled.div`
-height:90%;
-`
 const InputUsuario = styled.input`
 width: 30%;
-border:  solid red;
+`
+const CampoDosInputs = styled.div`
+width: 100%;
 `
 
 const InputMensagem = styled.input`
@@ -38,30 +26,38 @@ class TelaDoUsuario extends React.Component {
 
     MostrarUsuario = (event) => {
         let nomeDoUsario = event.target.value
-        this.setState ({NomeImpressoNoContainer: nomeDoUsario})
+        this.setState({ NomeImpressoNoContainer: nomeDoUsario })
         console.log(this.state.NomeImpressoNoContainer)
     }
 
     MostrarMensagem = (event) => {
         let mensagemUsuario = event.target.value
-        this.setState ({MensagemImpressaNoContainer: mensagemUsuario})
+        this.setState({ MensagemImpressaNoContainer: mensagemUsuario })
         console.log(this.state.MensagemImpressaNoContainer)
     }
 
-    EnviarMensagem= () => {
+    EnviarMensagem = () => {
+        const mensagens = {
+            usuario: this.state.NomeImpressoNoContainer,
+            mensagem: this.state.MensagemImpressaNoContainer
+        }
+        console.log(mensagens)
+        this.props.AdicionarMensagem(mensagens)
+
+        this.state.MensagemImpressaNoContainer = ""
 
     }
 
+
     render() {
         return (
-            <Container> conteúdo
-    <AreaDaMensagem> area da mensagem</AreaDaMensagem>
-                <CampoDosInputs>
-                    <InputUsuario onChange={this.MostrarUsuario} />
-                    <InputMensagem onChange={this.MostrarMensagem} />
-                    <BotaoEnviar onClick={this.EnviarMensagem}>Enviar</BotaoEnviar>
-                </CampoDosInputs>
-            </Container>
+
+            <CampoDosInputs>
+                <InputUsuario placeholder="Usuário" onChange={this.MostrarUsuario} />
+                <InputMensagem value={this.state.MensagemImpressaNoContainer} placeholder="Mensagem" onChange={this.MostrarMensagem} />
+                <BotaoEnviar onClick={this.EnviarMensagem}>Enviar</BotaoEnviar>
+            </CampoDosInputs>
+
 
         )
     }
